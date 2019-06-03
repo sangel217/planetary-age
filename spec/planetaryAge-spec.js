a068c009d017f72f87b1290c17a2e3e2eea5e997
Sarah Angel <sangel217@hotmail.com>
2019-06-03 00:11:16 -0700

    add constructor object to collect user gender and add to methods to calculate life expectancy based on users gender

--- spec/planetaryAge-spec.js
+++ spec/planetaryAge-spec.js
@@ -4,7 +4,7 @@ describe('PlanetaryAge', function(){
   let reusableAge;
 
   beforeEach(function(){
-    reusableAge = new PlanetaryAge ("1987, 7, 24");
+    reusableAge = new PlanetaryAge ("1987, 7, 24", "male");
   });
 
   it('should show how beforeEach() works', function() {
@@ -12,22 +12,23 @@ describe('PlanetaryAge', function(){
   });
 
   it('should calculate a users age on Earth', function() {
-    expect(reusableAge.checkEarth()).toEqual(32);
+    console.log(reusableAge.checkEarth());
+    expect(reusableAge.checkEarth()).toEqual([32, 68]);
   });
 
   it('should calculate a users age on Mercury', function() {
-    expect(reusableAge.checkMercury()).toEqual(133);
+    expect(reusableAge.checkMercury()).toEqual([133, 283]);
   });
 
   it('should calculate a users age on Venus', function() {
-    expect(reusableAge.checkVenus()).toEqual(51);
+    expect(reusableAge.checkVenus()).toEqual([51, 109]);
   });
 
   it('should calculate a users age on Mars', function() {
-    expect(reusableAge.checkMars()).toEqual(17);
+    expect(reusableAge.checkMars()).toEqual([17, 36]);
   });
 
   it('should calculate a users age on Jupiter', function() {
-    expect(reusableAge.checkJupiter()).toEqual(2);
+    expect(reusableAge.checkJupiter()).toEqual([2, 5]);
   });
 });
--- src/planetaryAge.js
+++ src/planetaryAge.js
@@ -1,39 +1,75 @@
-export function PlanetaryAge(dob)
+export function PlanetaryAge(dob, gender)
 {
   this.dob = dob;
   this.currentDate = new Date();
+  this.gender = gender;
 }
 
 PlanetaryAge.prototype.checkEarth = function(){
   let d = new Date(this.dob);
   let earthAge = this.currentDate.getFullYear() - d.getFullYear();
-  return earthAge;
+  let maleExpectancy = 0;
+  let femaleExpectancy = 0;
+  if(this.gender == "male"){
+    maleExpectancy += 68;
+  } else{
+    femaleExpectancy += 72;
+  }
+  return [earthAge, (maleExpectancy || femaleExpectancy)];
 }
 
 PlanetaryAge.prototype.checkMercury = function() {
   let d = new Date(this.dob);
   let earthAge = this.currentDate.getFullYear() - d.getFullYear();
   let mercuryAge = earthAge/0.24;
-  return parseInt(mercuryAge);
+  let maleExpectancy = 0;
+  let femaleExpectancy = 0;
+  if(this.gender == "male"){
+    maleExpectancy += 283;
+  } else{
+    femaleExpectancy += 300;
+  }
+  return [parseInt(mercuryAge), (maleExpectancy || femaleExpectancy)];
 }
 
 PlanetaryAge.prototype.checkVenus = function() {
   let d = new Date(this.dob);
   let earthAge = this.currentDate.getFullYear() - d.getFullYear();
   let venusAge = earthAge/0.62;
-  return parseInt(venusAge);
+  let maleExpectancy = 0;
+  let femaleExpectancy = 0;
+  if(this.gender == "male"){
+    maleExpectancy += 109;
+  } else{
+    femaleExpectancy += 116;
+  }
+  return [parseInt(venusAge), (maleExpectancy || femaleExpectancy)];
 }
 
 PlanetaryAge.prototype.checkMars = function() {
   let d = new Date(this.dob);
   let earthAge = this.currentDate.getFullYear() - d.getFullYear();
   let marsAge = earthAge/1.88;
-  return parseInt(marsAge);
+  let maleExpectancy = 0;
+  let femaleExpectancy = 0;
+  if(this.gender == "male"){
+    maleExpectancy += 36;
+  } else{
+    femaleExpectancy += 38;
+  }
+  return [parseInt(marsAge), (maleExpectancy || femaleExpectancy)];
 }
 
 PlanetaryAge.prototype.checkJupiter = function() {
   let d = new Date(this.dob);
   let earthAge = this.currentDate.getFullYear() - d.getFullYear();
   let jupiterAge = earthAge/11.86;
-  return parseInt(jupiterAge);
+  let maleExpectancy = 0;
+  let femaleExpectancy = 0;
+  if(this.gender == "male"){
+    maleExpectancy += 5;
+  } else{
+    femaleExpectancy += 6;
+  }
+  return [parseInt(jupiterAge), (maleExpectancy || femaleExpectancy)];
 }
